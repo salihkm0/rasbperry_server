e341b67751725a2cb2b881995d28c840eaf219d8
Muhammed Salih Km <salihkm000@gmail.com>
2024-11-14 12:52:58 +0530

    firefox

--- server/utils/openBrowser.js
+++ server/utils/openBrowser.js
@@ -1,8 +1,20 @@
+// import open from "open";
+
+// export default async function openBrowser() {
+//   try {
+//     await open('http://localhost:5173/', { app: { name: 'google chrome', arguments: ["--start-fullscreen"] } });
+//     console.log("✔ Browser opened in fullscreen mode");
+//   } catch (error) {
+//     console.error("✖ Failed to open browser:", error);
+//   }
+// }
+
+
 import open from "open";
 
 export default async function openBrowser() {
   try {
-    await open('http://localhost:5173/', { app: { name: 'google chrome', arguments: ["--start-fullscreen"] } });
+    await open('http://localhost:5173/', { app: { name: 'firefox', arguments: ["--start-fullscreen"] } });
     console.log("✔ Browser opened in fullscreen mode");
   } catch (error) {
     console.error("✖ Failed to open browser:", error);
